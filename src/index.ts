5688e9a007800a2c29f3de49fa3177d496af7525
oleg90990 <oleg909902@yandex.ru>
2020-08-19 19:19:40 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Axios from './axios';
 
-export * from './enum'
-export * from './api/Ads/types' 
-export * from './api/Dictionaries/types' 
-export * from './api/User/types' 
-export * from './api/Vk/types' 
+export { Mode } from './enum'
+export * from './api/Ads/types'
+export * from './api/Dictionaries/types'
+export * from './api/User/types'
+export * from './api/Vk/types'
 
 export default Axios
