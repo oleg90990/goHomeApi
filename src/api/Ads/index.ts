a063de97edb44737d5b78711805ef5ffc668c619
oleg90990 <oleg909902@yandex.ru>
2020-08-21 18:19:47 +0300

    fx

--- src/api/Ads/index.ts
+++ src/api/Ads/index.ts
@@ -16,23 +16,23 @@ export default class Ads {
     }
 
     public async create(params: IPostCreatedBody): Promise<AxiosResponse<IItem>> {
-        return this.axiosInstance.post('ads/store', params);
+        return this.axiosInstance.post('posts/store', params);
     }
 
     public async update(params: IPostUpdateBody): Promise<AxiosResponse<IItem>> {
-        return this.axiosInstance.post('ads/update', params);
+        return this.axiosInstance.post('posts/update', params);
     }
     
     public async find(params: IStateSearchBody, sortBy: Sortby, page: number): Promise<AxiosResponse<IFindBody>> {
-        return this.axiosInstance.post('ads/find', { ...params, sortBy, page });
+        return this.axiosInstance.post('posts/find', { ...params, sortBy, page });
     }
 
     public async me(): Promise<AxiosResponse<IItem[]>> {
-        return this.axiosInstance.get('ads/me');
+        return this.axiosInstance.get('posts/me');
     }
 
     public async publish(id: number, active: boolean): Promise<AxiosResponse<IItem>> {
-        return this.axiosInstance.post(`ads/publish`, {
+        return this.axiosInstance.post(`posts/publish`, {
             active,
             id
         });
